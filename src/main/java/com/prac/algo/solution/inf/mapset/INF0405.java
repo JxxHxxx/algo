e7c1f6ecf1561aded96e68b63f29ee698e46f140
JxxHxxx <leesin5498@naver.com>
2024-06-01 21:18:03 +0900

    FEAT 인프런 treeSet

---
+++ src/main/java/com/prac/algo/solution/inf/mapset/INF0405.java
@@ -0,0 +1,38 @@
+package com.prac.algo.solution.inf.mapset;
+
+
+import java.util.*;
+
+/**
+ * K번째 큰 수, TreeSet = 정렬 및 중복 제거
+ */
+
+
+public class INF0405 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        List<Integer> numberCards = new ArrayList<>();
+
+        int iter = sc.nextInt();
+        int target = sc.nextInt();
+
+        for (int i = 0; i < iter; i++) {
+            numberCards.add(sc.nextInt());
+        }
+
+        TreeSet<Integer> results = new TreeSet<>(Collections.reverseOrder());
+
+        for (int i = 0; i < iter; i++) {
+            for (int j = i + 1; j < iter; j++) {
+                for (int k = j + 1; k < iter; k++) {
+                    results.add(numberCards.get(i) + numberCards.get(j) + numberCards.get(k));
+                }
+            }
+        }
+
+        Integer result = results.stream().toList().get(target - 1);
+        System.out.println(result);
+    }
+}
